fc3caaedef167a74bfe43beb4a78ad862652cc6e
danie1garrido <daniel.garrido.info@gmail.com>
2017-09-14 23:45:24 +0100

    add create on controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,6 +1,24 @@
 class DevicesController < ApplicationController
+
   def index
     devices = Device.all
     render json: { status:'SUCCESS', message: 'Loaded devices', data:devices }, status: :ok
   end
+
+  def create
+    device = Device.new(device_params)
+    if device.save
+      render json: { status:'SUCCESS', message: 'Saved device', data:device }, status: :ok
+    else
+      render json: { status:'ERROR', message: 'Device not saved', data:device.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def device_params
+    params.permit(:model, :make, :category_id)
+  end
+
+
 end
