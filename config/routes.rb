e30bc030fc87095aa22d6c6bb6e6abd90d59002b
danie1garrido <daniel.garrido.info@gmail.com>
2017-09-14 23:16:59 +0100

    add Category Model and controller

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  scope path: 'api/v1' do
+    resources :categories
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
